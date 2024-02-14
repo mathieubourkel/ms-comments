e1be8eba9cd6e2b2f76746288471e8df795c8be8
Laetitia Ashry <ashry.laetitia@gmail.com>
2024-02-14 16:23:20 +0100

    install nats

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.NATS,
+    options: {
+      url: 'nats://localhost:4222',
+    },
+  });
+  await app.listen()
 }
 bootstrap();
